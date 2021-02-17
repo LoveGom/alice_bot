c9896adc15988cde187280ae8ce42b6b7f396975
LoveGom <qpig9302@naver.com>
2021-02-17 16:10:05 +0900

    온도 기능 추가

--- gomtang.py
+++ gomtang.py
@@ -4,6 +4,7 @@ import psutil
 import pybithumb
 from discord.ext import commands
 from datetime import datetime
+from gpiozero import CPUTemperature
 
 
 
@@ -96,8 +97,9 @@ async def 정보(ctx):
     percent = memory.percent #메모리 퍼센트 계산
     total = round(memory.total/1024**3, 1) # 총 메모리를 계산 후 소수점 1자리 까지만 반올림
     distotal = round(memory.total/1024**3) # "" 반올림 (소수점 X)
+    cpu = CPUTemperature() #cpu 온도 확인
     embedVar = discord.Embed(title="정보", description="시스템 정보를 표시합니다.", color=0x90EE90) 
-    embedVar.add_field(name="CPU 사용량 :", value=f"{psutil.cpu_percent()}%", inline=False) #embed에 각각 변수를 넣습니다
+    embedVar.add_field(name="CPU 사용량 :", value=f"{psutil.cpu_percent()}% ({cpu.temperature}°C)", inline=False) #embed에 각각 변수를 넣습니다
     embedVar.add_field(name="메모리 사용량 :", value=f"{distotal}GB 중 {round(total - avail, 1)}GB 사용 중 ({avail}GB 사용 가능 ({percent}%))", inline=False)
     embedVar.set_thumbnail(url="https://i.ibb.co/dW3kb01/dd1.png")
     await ctx.send(embed=embedVar) #embed를 출력합니다
